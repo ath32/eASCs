8a1798f7faa4e8bd9c40f314ec9018c3df3a00b4
ath32 <43788987+ath32@users.noreply.github.com>
2020-03-05 17:01:50 +0000

    Update PGLS.R

--- 0_Clean_R_Scripts/PGLS.R
+++ 0_Clean_R_Scripts/PGLS.R
@@ -6,13 +6,21 @@ library(caper)
 
 ### PGLS ANALYSIS ###
 
+#set working directory
 setwd("/Users/atho/Documents/2_Project_FSM")
+#load data
 data<-read.table("9_Modeling/TXT/data.txt",header=F,sep="\t", stringsAsFactors = F)
+#load tree
 tree<-read.nexus("9_Modeling/TREE/species_list_mlr.nwk3.tre")
+#set column names of data (varies depending on the data used, of course)
 names(data) <- c("Species", "ASC", "Ne", "Cellularity", 'GC', 'Gene_length', 'IGR')
+#set row names to species
 rownames(data) <- data$Species
+#create comparative data
 comp.data<-comparative.data(tree, data, names.col="Species", vcv.dim=2, warn.dropped=TRUE)
+#run pgls model for the desired variables (depending on the particular test)
 model<-pgls(ASC~IGR, data=comp.data, lambda = "ML")
+#summarise model and assess coefficients
 summary(model)
 
 
